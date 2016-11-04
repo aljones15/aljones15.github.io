4118db5ce37e93f6410fc03cd50e57e03d50df05
liminal18 <aljones15@gmail.com>
2016-11-04 11:06:23 +0700

    working towards a better elm model

--- index.elm
+++ index.elm
@@ -4,50 +4,52 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 main =
-  App.beginnerProgram { model = model, view = view, update = update }
+  App.beginnerProgram { model = head, view = view, update = update }
 
-type alias Model =
+type alias Head =
   { name : String
-  , password : String
-  , passwordAgain : String
+  , github : String
+  , linkedin : String
+  , p_img : String
   }
 
-model : Model
-model =
-  Model "" "" ""
+
+head : Head
+head =
+  Head "" "" "" ""
 
 type Msg
   = Name String
   | Password String
   | PasswordAgain String
 
-update : Msg -> Model -> Model
-update msg model =
+update : Msg -> Head -> Head
+update msg head =
   case msg of
     Name name ->
-      { model | name = name }
+      { head | name = name }
     Password password ->
-      { model | password = password }
+      { head | github = password }
     PasswordAgain password ->
-      { model | passwordAgain = password }
+      { head | linkedin = password }
 
-view : Model -> Html Msg
-view model =
+view : Head -> Html Msg
+view head =
   div []
     [ input [ type' "text", placeholder "Name", onInput Name ] []
     , input [ type' "password", placeholder "Password", onInput Password ] []
     , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain
     ] []
-      , viewValidation model
+      , viewValidation head
       ]
 
-viewValidation : Model -> Html msg
-viewValidation model =
+viewValidation : Head -> Html msg
+viewValidation head =
   let
     (color, message) =
-      if model.password == model.passwordAgain then
+      if head.github == head.linkedin then
         ("green", "OK")
-      else 
+      else
         ("red", "Passwords do not match!")
   in
     div [ style [("color", color)]] [text message ]
